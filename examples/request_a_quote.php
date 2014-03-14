97470793af7039619de1c0001789811ec95b7c3c
Florent Piteau <florent.piteau@sylogix.net>
2014-03-14 15:50:31 +0100

    City is mandatory

--- examples/request_a_quote.php
+++ examples/request_a_quote.php
@@ -7,8 +7,8 @@ use \Lce\Resource\Quote;
 Lce::configure('login', 'password', 'staging');
 
 $params = array(
-  'shipper' => array('postal_code' => '31300','country' => 'FR'),
-  'recipient' => array('postal_code' => '06800','country' => 'FR','is_a_company' => true),
+  'shipper' => array('postal_code' => '31300', 'city' => 'Toulouse', 'country' => 'FR'),
+  'recipient' => array('postal_code' => '06800', 'city' => 'Cagnes-sur-mer', 'country' => 'FR','is_a_company' => true),
   'parcels' => array(
     array('length' => 10, 'height' => 10, 'width' => 10, 'weight' => 1),
     array('length' => 20, 'height' => 20, 'width' => 20, 'weight' => 2)
